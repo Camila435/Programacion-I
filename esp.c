2a50ad73e876cac93b4db72b8484f1dbe5de9833
Camila435 <camiquintero365@gmail.com>
2025-04-25 14:15:11 -0300

    Agregando

---
+++ esp.c
@@ -0,0 +1,95 @@
+/* formateadores o especificadores
+%d o %i enteros
+%f float(decimales)
+%.2f float(2decimales despues de la coma)
+%c char(caracteres)
+%s cadenas de texto
+%lf long double
+%u entero sin signo en decimal.
+*/
+
+/*
+salto de lineas 
+\n salto de linea
+\t tabulacion (espacio grande)
+\\ muestra barra invertida
+\" muestra comillas
+
+*/
+
+/*
+Funciones de entrada y salida:
+Getchar y putchar: permiten la transferencia de carcateres individuales hacia dentro y
+hacia fuera de la computadora.
+Gets y puts: permite la entrada y salida de cadenas de caracteres.
+Scanf y printf: permiten la transferencia de caracteres individuales, valores numéricos y
+cadenas de caracteres.
+
+
+*/
+
+
+/*
+Secuencia de escape: Las secuencias de escape son cadenas de caracteres que tienen distintos significados
+dependiendo de la cadena que se utilice.
+
+
+*/
+
+/*
+sintaxis del if
+if(condicion){
+codigo que se ejecuta si es verdadera la condicion
+}
+
+sintaxis if else
+
+if (condicion){
+se ejecuta la condicion si es verdadera o si es falsa se ejecuta el siguiente else
+} else{
+    se ejecuta si es falso si no sale 
+}
+ 
+
+sintaxis de if else anidado
+
+if(condicion1){
+se ejecuta condicion 1 si es verdad
+  if(condicion2){
+  se ejecuta si condicion2 tambien es verdadera
+  }else{ codigo si condicion 2 es false
+} else{ codifo si condicion1 es falso
+   }
+
+   sintaxis while
+   while(condicion){ si es verdad se ejecuta
+   }
+
+sintaxis del for
+for(inicia; condicion; actualiza(aumenta el contador)){
+se ejecuta el codigo siempr ey cuando la condicion siga siendo verdadera.
+}
+
+sintaxis do-while
+
+do{
+codigo que se ejecuta 1 vez y se continuara ejecutando siempre que la condicion del while sea verdadera
+} while(condicion); va a parar cuando se cumpla la condicion
+
+sintaxis switch
+
+swhitch(valor)
+{ 
+case valor1: <sentancias> break;
+case valor2: <sentencias> break;
+...
+default:<sentencias>
+}
+
+*/
+
+/*operadores logicos
+! not(no es v)
+&& and(y)
+|| or(o)
+*/
